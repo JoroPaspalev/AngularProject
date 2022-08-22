108ce20a1ed4f030c63a33f3f95a91d8966a4fa0
Mariyan Marinov <mariyan.marinov@quest-global.com>
2022-08-22 13:56:58 +0300

    NGRX-DEV-TOOLS

--- Ngrx-Redux/ngrxFire/src/app/app.module.ts
+++ Ngrx-Redux/ngrxFire/src/app/app.module.ts
@@ -8,17 +8,22 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './reducers/simple.reducer';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot(reducers),FormsModule
-    
+    AppRoutingModule,    
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
